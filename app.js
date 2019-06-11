027edfe862acae928fefc9f92bc3e766adb49107
Jeffrey Chen <chenjeff53@gmail.com>
2019-06-11 15:34:47 -0400

    form changes

--- app.js
+++ app.js
@@ -19,8 +19,42 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(function(req,res,next){
+  console.log("about to look for post routes?!?")
+  console.dir(req.headers)
+  next()
+});
+
+function processFormData(req,res,next){
+  res.render('formdata',
+    {title:"Form Data",inputEmail4:req.body.inputEmail4, inputPassword4:req.body.inputPassword4, inputAddress:req.body.inputAddress, inputAddress2:req.body.inputAddress2, inputCity:req.body.inputCity, inputState:req.body.inputState, inputZip:req.body.inputZip});
+  }
+
+
+
+
+app.get('/myform', function(req, res, next) {
+  res.render('myform',{title:"Form Demo"});
+});
+
+app.use(function(req,res,next){
+  console.log("about to look for post routes?!?")
+  console.dir(req.body)
+  next()
+});
+
+app.post('/processform', function(req, res, next) {
+  console.log("In the processform code ..")
+  console.dir(req.body)
+  console.log("req.body.inputAddress = "+req.body.inputAddress)
+  res.render('formdata',
+  {title:"Form Data",inputEmail4:req.body.inputEmail4, inputPassword4:req.body.inputPassword4, inputAddress:req.body.inputAddress, inputAddress2:req.body.inputAddress2, inputCity:req.body.inputCity, inputState:req.body.inputState, inputZip:req.body.inputZip})
+});
+
+// app.use('/', indexRouter);  // this is how we use a router to handle the / path
+// but here we are more direct
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
